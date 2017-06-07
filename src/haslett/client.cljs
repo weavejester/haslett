456e4424ac4715fd437a1391c03fad72e806b322
James Reeves <jreeves@weavejester.com>
2017-06-07 19:09:49 +0100

    Add JSON source and sink

--- src/haslett/client.cljs
+++ src/haslett/client.cljs
@@ -3,12 +3,25 @@
             [cljs.reader :as edn])
   (:require-macros [cljs.core.async.macros :refer [go-loop]]))
 
+(defn- chan-fn [xf]
+  (fn chan
+    ([]                          (chan 1))
+    ([buf-or-n]                  (chan buf-or-n identity))
+    ([buf-or-n xform]            (chan buf-or-n xform nil))
+    ([buf-or-n xform ex-handler] (a/chan buf-or-n (comp xf xform) ex-handler))))
+
+(def edn-source  (chan-fn (map edn/read-string)))
+(def edn-sink    (chan-fn (map pr-str)))
+
+(def json-source (chan-fn (map #(js->clj (js/JSON.parse %)))))
+(def json-sink   (chan-fn (map #(js/JSON.stringify (clj->js %)))))
+
 (defn websocket
   ([url]
    (websocket url {}))
   ([url options]
-   (let [source (:source options (a/chan 1 (map edn/read-string)))
-         sink   (:sink options   (a/chan 1 (map pr-str)))
+   (let [source (:source options (edn-source))
+         sink   (:sink options   (edn-sink))
          socket (js/WebSocket. url)
          return (a/promise-chan)]
      (aset socket "onopen"    (fn [_] (a/put! return {:source source, :sink sink})))
--- test/haslett/client_test.cljs
+++ test/haslett/client_test.cljs
@@ -5,8 +5,19 @@
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (deftest test-ws
-  (async done
-    (go (let [{:keys [sink source]} (<! (haslett/websocket "ws://echo.websocket.org"))]
-          (>! sink {:hello "World"})
-          (is (= (<! source) {:hello "World"}))
-          (done)))))
+  (testing "defaults"
+    (async done
+      (go (let [{:keys [sink source]} (<! (haslett/websocket "ws://echo.websocket.org"))]
+            (>! sink {:hello "World"})
+            (is (= (<! source) {:hello "World"}))
+            (done)))))
+
+  (testing "json formatting"
+    (async done
+      (go (let [{:keys [sink source]}
+                (<! (haslett/websocket "ws://echo.websocket.org"
+                                       {:source (haslett/json-source)
+                                        :sink   (haslett/json-sink)}))]
+            (>! sink {:hello "World"})
+            (is (= (<! source) {"hello" "World"}))
+            (done))))))
