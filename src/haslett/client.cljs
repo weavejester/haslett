b05f4f59e05b015a7ddb31121e9fcd63b321bfc3
James Reeves <jreeves@weavejester.com>
2017-06-07 17:56:39 +0100

    Add basic WebSocket API

--- src/haslett/client.cljs
+++ src/haslett/client.cljs
@@ -1,6 +1,23 @@
-(ns haslett.client)
+(ns haslett.client
+  (:refer-clojure :exclude [send]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn websocket
+  ([url]
+   (websocket url {}))
+  ([url options]
+   (let [sock (js/WebSocket. url)]
+     (when-let [on-open    (:on-open options)]
+       (aset sock "onopen"    (fn [evt] (on-open sock evt))))
+     (when-let [on-close   (:on-close options)]
+       (aset sock "onclose"   (fn [evt] (on-close sock evt))))
+     (when-let [on-message (:on-message options)]
+       (aset sock "onmessage" (fn [evt] (on-message sock (.-data evt) evt))))
+     (when-let [on-error   (:on-error options)]
+       (aset sock "onerror"   (fn [evt] (on-error sock evt))))
+     sock)))
+
+(defn send [ws data]
+  (.send ws data))
+
+(defn close [ws]
+  (.close ws))
--- test/haslett/client_test.cljs
+++ test/haslett/client_test.cljs
@@ -1,7 +1,21 @@
 (ns haslett.client-test
   (:require [cljs.test :refer-macros [deftest is testing]]
-            [haslett.client]))
+            [haslett.client :as haslett]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-ws
+  (let [events (atom [])]
+    (haslett/websocket
+     "ws://echo.websocket.org"
+     {:on-open    (fn [ws]
+                    (swap! events conj [:open])
+                    (haslett/send ws "Hello World"))
+      :on-message (fn [ws data]
+                    (swap! events conj [:message])
+                    (haslett/close ws))
+      :on-close   (fn [ws]
+                    (swap! events conj [:close]))})
+    (js/setTimeout
+     #(is (= @events [[:open]
+                      [:message "Hello World"]
+                      [:close]]))
+     1000)))
