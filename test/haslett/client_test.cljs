a7ea3509ab60e696fe15f6ed278d7728d65c6c69
James Reeves <jreeves@weavejester.com>
2017-06-08 17:29:14 +0100

    Fix async tests

--- test/haslett/client_test.cljs
+++ test/haslett/client_test.cljs
@@ -5,42 +5,41 @@
             [haslett.format :as fmt])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(deftest test-ws
-  (testing "defaults"
-    (async done
-      (go (let [stream (<! (ws/connect "ws://echo.websocket.org"))]
-            (>! (:sink stream) "Hello World")
-            (is (= (<! (:source stream)) "Hello World"))
-            (ws/close stream)
-            (done)))))
+(deftest test-defaults
+  (async done
+    (go (let [stream (<! (ws/connect "ws://echo.websocket.org"))]
+          (>! (:sink stream) "Hello World")
+          (is (= (<! (:source stream)) "Hello World"))
+          (ws/close stream)
+          (done))))) 
 
-  (testing "transit formatting"
-    (async done
-      (go (let [stream (<! (ws/connect "ws://echo.websocket.org" {:format fmt/transit}))]
-            (>! (:sink stream) {:hello "World"})
-            (is (= (<! (:source stream)) {:hello "World"}))
-            (ws/close stream)
-            (done)))))
+(deftest test-transit
+  (async done
+    (go (let [stream (<! (ws/connect "ws://echo.websocket.org" {:format fmt/transit}))]
+          (>! (:sink stream) {:hello "World"})
+          (is (= (<! (:source stream)) {:hello "World"}))
+          (ws/close stream)
+          (done)))))
 
-  (testing "edn formatting"
-    (async done
-      (go (let [stream (<! (ws/connect "ws://echo.websocket.org" {:format fmt/edn}))]
-            (>! (:sink stream) {:hello "World"})
-            (is (= (<! (:source stream)) {:hello "World"}))
-            (ws/close stream)
-            (done)))))
+(deftest test-edn
+  (async done
+    (go (let [stream (<! (ws/connect "ws://echo.websocket.org" {:format fmt/edn}))]
+          (>! (:sink stream) {:hello "World"})
+          (is (= (<! (:source stream)) {:hello "World"}))
+          (ws/close stream)
+          (done)))))
 
-  (testing "json formatting"
-    (async done
-      (go (let [stream (<! (ws/connect "ws://echo.websocket.org" {:format fmt/json}))]
-            (>! (:sink stream) {:hello "World"})
-            (is (= (<! (:source stream)) {"hello" "World"}))
-            (ws/close stream)
-            (done)))))
+(deftest test-json
+  (async done
+    (go (let [stream (<! (ws/connect "ws://echo.websocket.org" {:format fmt/json}))]
+          (>! (:sink stream) {:hello "World"})
+          (is (= (<! (:source stream)) {"hello" "World"}))
+          (ws/close stream)
+          (done)))))
 
-  (testing "close status"
-    (async done
-      (go (let [stream (<! (ws/connect "ws://echo.websocket.org"))]
-            (is (= (<! (ws/close stream))      {:code 1005, :reason ""}))
-            (is (= (<! (:close-status stream)) {:code 1005, :reason ""}))
-            (done))))))
+(deftest test-close
+  (async done
+    (go (let [stream (<! (ws/connect "ws://echo.websocket.org"))]
+          (is (= (<! (ws/close stream))      {:code 1005, :reason ""}))
+          (is (= (<! (:close-status stream)) {:code 1005, :reason ""}))
+          (done)))))
