8eaa7c54e1cf686e25a6ce3db28dbab1a7296800
James Reeves <jreeves@weavejester.com>
2017-06-09 14:04:23 +0100

    Change :test profile to :dev profile

--- project.clj
+++ project.clj
@@ -8,16 +8,16 @@
                  [org.clojure/core.async "0.3.443"]
                  [com.cognitect/transit-cljs "0.8.239"]]
   :plugins [[lein-cljsbuild "1.1.6"]]
-  :aliases {"test" ["with-profile" "test" "run" "-m" "haslett.test-runner"]}
+  :aliases {"test" ["run" "-m" "haslett.test-runner"]}
   :profiles
-  {:test {:dependencies [[doo "0.1.7"]
-                         [http-kit "2.2.0"]]
-          :prep-tasks   ["compile" ["cljsbuild" "once"]]
-          :cljsbuild
-          {:builds
-           {:test
-            {:source-paths ["src" "test"]
-             :compiler {:output-to "target/main.js"
-                        :output-dir "target"
-                        :main haslett.test-runner
-                        :optimizations :none}}}}}})
+  {:dev {:dependencies [[doo "0.1.7"]
+                        [http-kit "2.2.0"]]
+         :prep-tasks   ["compile" ["cljsbuild" "once"]]
+         :cljsbuild
+         {:builds
+          {:test
+           {:source-paths ["src" "test"]
+            :compiler {:output-to "target/main.js"
+                       :output-dir "target"
+                       :main haslett.test-runner
+                       :optimizations :none}}}}}})
