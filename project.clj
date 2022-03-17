3ca264b6bf4c8206e506262a4f78602bd69d67aa
James Reeves <jreeves@weavejester.com>
2022-03-17 18:13:51 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,15 +3,15 @@
   :url "https://github.com/weavejester/haslett"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/clojurescript "1.10.520"]
-                 [org.clojure/core.async "0.4.490"]
-                 [com.cognitect/transit-cljs "0.8.256"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/clojurescript "1.11.4"]
+                 [org.clojure/core.async "1.5.648"]
+                 [com.cognitect/transit-cljs "0.8.269"]]
   :plugins [[lein-cljsbuild "1.1.7"]]
   :aliases {"test" ["run" "-m" "haslett.test-runner"]}
   :profiles
   {:dev {:dependencies [[doo "0.1.11"]
-                        [http-kit "2.3.0"]]
+                        [http-kit "2.5.3"]]
          :prep-tasks   ["compile" ["cljsbuild" "once"]]
          :cljsbuild
          {:builds
