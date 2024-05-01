c726d930f00223f4c896e6a7ee8515b9d6247070
James Reeves <jreeves@weavejester.com>
2024-05-01 15:37:54 +0100

    Rename :source to :in and :sink to :out
    Naming the reader and writer channels in terms of input and output
    makes more intuitive sense when dealing with WebSockets.
    
    This is a breaking change.

--- src/haslett/client.cljs
+++ src/haslett/client.cljs
@@ -15,14 +15,14 @@
 
     :socket       - contains the WebSocket object
     :close-status - a promise channel that contains the final close status
-    :source       - a core.async channel to read from
-    :sink         - a core.async channel to write to
+    :in           - a core.async channel to read from
+    :out          - a core.async channel to write to
 
   Takes the following options:
 
     :format      - a formatter from haslett.format
-    :source      - a custom channel to use as the source
-    :sink        - a custom channel to use as the sink
+    :in          - a custom channel to use as the reader
+    :out         - a custom channel to use as the writer
     :protocols   - passed to the WebSocket, a vector of protocol strings
     :binary-type - passed to the WebSocket, may be :blob or :arraybuffer
     :close-chan? - true if channels should be closed if WebSocket is closed
@@ -35,23 +35,25 @@
   ([url options]
    (let [protocols (into-array (:protocols options []))
          socket    (js/WebSocket. url protocols)
-         source    (:source options (a/chan))
-         sink      (:sink   options (a/chan))
+         in        (:in options (a/chan))
+         out       (:out options (a/chan))
          format    (:format options fmt/identity)
          status    (a/promise-chan)
          return    (a/promise-chan)
          close?    (:close-chan? options true)
-         stream    {:socket socket, :source source, :sink sink, :close-status status}]
+         stream    {:socket socket, :in in, :out out, :close-status status}]
      (set! (.-binaryType socket) (name (:binary-type options :arraybuffer)))
      (set! (.-onopen socket)     (fn [_] (a/put! return stream)))
-     (set! (.-onmessage socket)  (fn [e] (a/put! source (fmt/read format (.-data e)))))
-     (set! (.-onclose socket)    (fn [e]
-                                   (a/put! status {:reason (.-reason e), :code (.-code e)})
-                                   (when close? (a/close! source))
-                                   (when close? (a/close! sink))
-                                   (a/put! return stream)))
+     (set! (.-onmessage socket)
+           (fn [e] (a/put! in (fmt/read format (.-data e)))))
+     (set! (.-onclose socket)
+           (fn [e]
+             (a/put! status {:reason (.-reason e), :code (.-code e)})
+             (when close? (a/close! in))
+             (when close? (a/close! out))
+             (a/put! return stream)))
      (go-loop []
-       (when-let [msg (<! sink)]
+       (when-let [msg (<! out)]
          (.send socket (fmt/write format msg))
          (recur))
        (close stream))
--- test/haslett/client_test.cljs
+++ test/haslett/client_test.cljs
@@ -9,32 +9,32 @@
   (async done
     (go (let [stream (<! (ws/connect "ws://localhost:3200"))]
           (is (ws/connected? stream))
-          (>! (:sink stream) "Hello World")
-          (is (= (<! (:source stream)) "Hello World"))
+          (>! (:out stream) "Hello World")
+          (is (= (<! (:in stream)) "Hello World"))
           (ws/close stream)
           (done))))) 
 
 (deftest test-transit
   (async done
     (go (let [stream (<! (ws/connect "ws://localhost:3200" {:format fmt/transit}))]
-          (>! (:sink stream) {:hello "World"})
-          (is (= (<! (:source stream)) {:hello "World"}))
+          (>! (:out stream) {:hello "World"})
+          (is (= (<! (:in stream)) {:hello "World"}))
           (ws/close stream)
           (done)))))
 
 (deftest test-edn
   (async done
     (go (let [stream (<! (ws/connect "ws://localhost:3200" {:format fmt/edn}))]
-          (>! (:sink stream) {:hello "World"})
-          (is (= (<! (:source stream)) {:hello "World"}))
+          (>! (:out stream) {:hello "World"})
+          (is (= (<! (:in stream)) {:hello "World"}))
           (ws/close stream)
           (done)))))
 
 (deftest test-json
   (async done
     (go (let [stream (<! (ws/connect "ws://localhost:3200" {:format fmt/json}))]
-          (>! (:sink stream) {:hello "World"})
-          (is (= (<! (:source stream)) {"hello" "World"}))
+          (>! (:out stream) {:hello "World"})
+          (is (= (<! (:in stream)) {"hello" "World"}))
           (ws/close stream)
           (done)))))
 
@@ -49,17 +49,17 @@
   (async done
     (go (let [stream (<! (ws/connect "ws://localhost:3201"))]
           (is (not (ws/connected? stream)))
-          (is (ap/closed? (:sink stream)))
-          (is (ap/closed? (:source stream)))
+          (is (ap/closed? (:out stream)))
+          (is (ap/closed? (:in stream)))
           (is (= (<! (:close-status stream)) {:code 1006, :reason ""}))
           (done)))))
 
 (deftest test-local-close
   (async done
     (go (let [stream (<! (ws/connect "ws://localhost:3200"))]
-          (a/close! (:sink stream))
+          (a/close! (:out stream))
           (is (= (<! (:close-status stream)) {:code 1000, :reason "Closed by creator"}))
-          (is (ap/closed? (:source stream)))
+          (is (ap/closed? (:in stream)))
           (done)))))
 
 (deftest test-chans-not-closed
@@ -67,6 +67,6 @@
     (go (let [stream (<! (ws/connect "ws://localhost:3200" {:close-chan? false}))]
           (ws/close stream)
           (is (= (<! (:close-status stream)) {:code 1000, :reason "Closed by creator"}))
-          (is (not (ap/closed? (:sink stream))))
-          (is (not (ap/closed? (:source stream))))
+          (is (not (ap/closed? (:out stream))))
+          (is (not (ap/closed? (:in stream))))
           (done)))))
